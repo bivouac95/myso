1f043ffa12c42e26528b3e6cd1c6e7401a37278b
Ivan T <144308917+bivouac95@users.noreply.github.com>
2024-05-15 16:16:16 +0900

    other things

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'mywhite': '#FFFFFC',
+      'myorange': '#E74833',
+      'mylight-gray': '#393838',
+      'mygray': '#222222',
+      'mydark-gray': '#191919',
+      'myblack': '#0C0B0B',
+      'mytransblack': '#0C0B0B80'
+    }
+  },
+  plugins: [],
+}
+
